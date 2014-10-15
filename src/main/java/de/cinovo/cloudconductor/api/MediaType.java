54cfa006f05731a565dc9c250f3633d7f0e42dcc
Philipp Sigloch <philipp.sigloch@tullius-walden.com>
2014-10-15 14:26:41 +0200

    refs #45 in CC-Server

--- src/main/java/de/cinovo/cloudconductor/api/MediaType.java
+++ src/main/java/de/cinovo/cloudconductor/api/MediaType.java
@@ -9,9 +9,9 @@ package de.cinovo.cloudconductor.api;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,10 +24,10 @@ package de.cinovo.cloudconductor.api;
  * Media types used and supported by the config server <br>
  * <br>
  * Copyright 2013 Cinovo AG<br>
- * 
+ *
  * @author psigloch
- * 
- * 
+ *
+ *
  */
 public interface MediaType {
 	
@@ -49,4 +49,6 @@ public interface MediaType {
 	static final String APPLICATION_JAVASCRIPT = "application/javascript";
 	/** font opentype MEDIA TYPE */
 	static final String FONT_OPENTYPE = "font/opentype";
+	/** Flash Shockwave TYPE */
+	static final String APPLICATION_FLASH = "application/x-shockwave-flash";
 }
