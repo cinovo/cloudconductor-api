32c4dca96bbf943990a3d99afa9ba279f8130a18
Marc Weise <marc.weise@cinovo.de>
2017-09-28 16:11:17 +0200

    Add interface for agent communication.

---
+++ src/main/java/de/cinovo/cloudconductor/api/interfaces/IAgent.java
@@ -0,0 +1,67 @@
+package de.cinovo.cloudconductor.api.interfaces;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import de.cinovo.cloudconductor.api.IRestPath;
+import de.cinovo.cloudconductor.api.MediaType;
+import de.cinovo.cloudconductor.api.model.AgentOption;
+import de.cinovo.cloudconductor.api.model.PackageState;
+import de.cinovo.cloudconductor.api.model.PackageStateChanges;
+import de.cinovo.cloudconductor.api.model.ServiceStates;
+import de.cinovo.cloudconductor.api.model.ServiceStatesChanges;
+
+/**
+ * 
+ * Copyright 2017 Cinovo AG<br>
+ * <br>
+ * 
+ * @author mweise
+ *
+ */
+@Path(IRestPath.AGENT)
+public interface IAgent {
+	
+	/**
+	 * @param template the template name
+	 * @param host the host name
+	 * @param rpmState the rpm state
+	 * @param uuid the UUID of the agent
+	 * @return list of instructions with changes to match the template
+	 */
+	@PUT
+	@Path(IRestPath.AGENT_PACKAGE_STATE)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public PackageStateChanges notifyPackageState(@PathParam(IRestPath.VAR_TEMPLATE) String template, @PathParam(IRestPath.VAR_HOST) String host, PackageState rpmState, @PathParam(IRestPath.VAR_UUID) String uuid);
+	
+	/**
+	 * @param template the template name
+	 * @param host the host name
+	 * @param serviceState the service state
+	 * @param uuid the UUID of the agent
+	 * @return list of instructions with changes to match the template
+	 */
+	@PUT
+	@Path(IRestPath.AGENT_SERVICE_STATE)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public ServiceStatesChanges notifyServiceState(@PathParam(IRestPath.VAR_TEMPLATE) String template, @PathParam(IRestPath.VAR_HOST) String host, ServiceStates serviceState, @PathParam(IRestPath.VAR_UUID) String uuid);
+	
+	/**
+	 * @param template the template name
+	 * @param host the host name
+	 * @param agent the agent name
+	 * @param uuidString the UUID of the agent
+	 * @return the current agent options
+	 */
+	@GET
+	@Path(IRestPath.AGENT_HEART_BEAT)
+	@Produces(MediaType.APPLICATION_JSON)
+	public AgentOption heartBeat(@PathParam(IRestPath.VAR_TEMPLATE) String template, @PathParam(IRestPath.VAR_HOST) String host, @PathParam(IRestPath.VAR_AGENT) String agent, @PathParam(IRestPath.VAR_UUID) String uuidString);
+	
+}
