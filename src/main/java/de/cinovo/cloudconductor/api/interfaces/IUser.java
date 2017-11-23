5da2cd859c1beeedfa604359e3fb100e67bfaf8a
Marc Weise <marc.weise@cinovo.de>
2017-11-23 10:19:10 +0100

    Add allowed roles for users and groups.

--- src/main/java/de/cinovo/cloudconductor/api/interfaces/IUser.java
+++ src/main/java/de/cinovo/cloudconductor/api/interfaces/IUser.java
@@ -1,9 +1,8 @@
 package de.cinovo.cloudconductor.api.interfaces;
 
-import de.cinovo.cloudconductor.api.MediaType;
-import de.cinovo.cloudconductor.api.model.PasswordChange;
-import de.cinovo.cloudconductor.api.model.User;
+import java.util.List;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -12,7 +11,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import java.util.List;
+
+import de.cinovo.cloudconductor.api.MediaType;
+import de.cinovo.cloudconductor.api.model.PasswordChange;
+import de.cinovo.cloudconductor.api.model.User;
 
 /**
  * Copyright 2017 Cinovo AG<br>
@@ -24,48 +26,53 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public interface IUser {
-
+	
 	/**
 	 * @return list of users
 	 */
 	@GET
+	@RolesAllowed({"VIEW_USERS", "EDIT_USER"})
 	List<User> getUsers();
-
+	
 	/**
 	 * @param user the user to save
 	 */
 	@PUT
+	@RolesAllowed({"EDIT_USER"})
 	void save(User user);
-
+	
 	/**
 	 * @param userName the user name
 	 */
 	@DELETE
 	@Path("/{username}")
+	@RolesAllowed({"EDIT_USER"})
 	void delete(@PathParam("username") String userName);
-
+	
 	/**
 	 * @param userName the user name
 	 * @return the user
 	 */
 	@POST
 	@Path("/{username}/authtoken")
+	@RolesAllowed({"EDIT_USER"})
 	User createAuthToken(@PathParam("username") String userName);
-
+	
 	/**
 	 * @param userName the user name
-	 * @param token    the token to revoke
+	 * @param token the token to revoke
 	 * @return the User which was changed
 	 */
 	@DELETE
 	@Path("/{username}/authtoken")
+	@RolesAllowed({"EDIT_USER"})
 	User revokeAuthToken(@PathParam("username") String userName, String token);
-
+	
 	/**
 	 * @param pwChange the password change request
 	 */
 	@PUT
 	@Path("/changepassword")
 	void changePassword(PasswordChange pwChange);
-
+	
 }
--- src/main/java/de/cinovo/cloudconductor/api/interfaces/IUserGroup.java
+++ src/main/java/de/cinovo/cloudconductor/api/interfaces/IUserGroup.java
@@ -1,8 +1,8 @@
 package de.cinovo.cloudconductor.api.interfaces;
 
-import de.cinovo.cloudconductor.api.MediaType;
-import de.cinovo.cloudconductor.api.model.UserGroup;
+import java.util.List;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -10,7 +10,9 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import java.util.List;
+
+import de.cinovo.cloudconductor.api.MediaType;
+import de.cinovo.cloudconductor.api.model.UserGroup;
 
 /**
  * Copyright 2017 Cinovo AG<br>
@@ -22,24 +24,27 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public interface IUserGroup {
-
+	
 	/**
 	 * @return list of usergroups
 	 */
 	@GET
+	@RolesAllowed({"VIEW_USERS", "EDIT_USER"})
 	List<UserGroup> getUserGroups();
-
+	
 	/**
 	 * @param userGroup the user group to save
 	 */
 	@PUT
+	@RolesAllowed({"EDIT_USER"})
 	void save(UserGroup userGroup);
-
+	
 	/**
 	 * @param userGroupName the user group name
 	 */
 	@DELETE
 	@Path("/{usergroupname}")
+	@RolesAllowed({"EDIT_USER"})
 	void delete(@PathParam("usergroupname") String userGroupName);
-
+	
 }
