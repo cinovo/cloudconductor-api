156ad0499d7696696c7a4a4a36a9ef2ca58f9bfa
Marc Weise <marc.weise@cinovo.de>
2017-11-30 14:07:47 +0100

    Add interface to refresh a JWT.

--- src/main/java/de/cinovo/cloudconductor/api/interfaces/IAuth.java
+++ src/main/java/de/cinovo/cloudconductor/api/interfaces/IAuth.java
@@ -1,12 +1,12 @@
 package de.cinovo.cloudconductor.api.interfaces;
 
-import de.cinovo.cloudconductor.api.MediaType;
-import de.cinovo.cloudconductor.api.model.Authentication;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
+import de.cinovo.cloudconductor.api.MediaType;
+import de.cinovo.cloudconductor.api.model.Authentication;
 
 /**
  * Copyright 2017 Cinovo AG<br>
@@ -18,7 +18,7 @@ import javax.ws.rs.Produces;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public interface IAuth {
-
+	
 	/**
 	 * @param auth the auth object
 	 * @return the jwt token for the user
@@ -26,12 +26,21 @@ public interface IAuth {
 	@PUT
 	@Path("/")
 	String auth(Authentication auth);
-
+	
+	/**
+	 * @param currentToken token which will expire soon
+	 * @return the new token
+	 */
+	@PUT
+	@Path("/refresh")
+	@Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
+	String refresh(String currentToken);
+	
 	/**
 	 * logout the current user and destroy his jwt token
 	 */
 	@PUT
 	@Path("/logout")
 	void logout();
-
+	
 }
--- src/main/java/de/cinovo/cloudconductor/api/model/Authentication.java
+++ src/main/java/de/cinovo/cloudconductor/api/model/Authentication.java
@@ -7,46 +7,73 @@ package de.cinovo.cloudconductor.api.model;
  * @author psigloch
  */
 public class Authentication {
-
+	
 	private String username;
 	private String password;
 	private String token;
-
+	
+	
+	/**
+	 * Create an empty authentication object
+	 */
+	public Authentication() {
+	}
+	
+	/**
+	 * Create new authentication object using token.
+	 * 
+	 * @param token the token to be used
+	 */
+	public Authentication(String token) {
+		this.token = token;
+	}
+	
+	/**
+	 * Create a new authentication object using username and password
+	 * 
+	 * @param username the username to be used
+	 * @param password the password to be used
+	 */
+	public Authentication(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
 	/**
 	 * @return the username
 	 */
 	public String getUsername() {
 		return this.username;
 	}
-
+	
 	/**
 	 * @param username the username to set
 	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
+	
 	/**
 	 * @return the password
 	 */
 	public String getPassword() {
 		return this.password;
 	}
-
+	
 	/**
 	 * @param password the password to set
 	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	
 	/**
 	 * @return the token
 	 */
 	public String getToken() {
 		return this.token;
 	}
-
+	
 	/**
 	 * @param token the token to set
 	 */
