342caf53cebc0eb1ef986fafbc5fd37aa133d3b2
Marc Weise <marc.weise@cinovo.de>
2017-12-06 10:27:02 +0100

    Enable settings to be read without login.
    - otherwise name of cloudconductor server could not be shown before login

--- src/main/java/de/cinovo/cloudconductor/api/interfaces/ISettings.java
+++ src/main/java/de/cinovo/cloudconductor/api/interfaces/ISettings.java
@@ -1,15 +1,15 @@
 package de.cinovo.cloudconductor.api.interfaces;
 
-import de.cinovo.cloudconductor.api.IRestPath;
-import de.cinovo.cloudconductor.api.MediaType;
-import de.cinovo.cloudconductor.api.model.Settings;
-
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
+import de.cinovo.cloudconductor.api.IRestPath;
+import de.cinovo.cloudconductor.api.MediaType;
+import de.cinovo.cloudconductor.api.model.Settings;
 
 /**
  * Copyright 2017 Cinovo AG<br>
@@ -26,7 +26,6 @@ public interface ISettings {
 	 * @return set of service objects
 	 */
 	@GET
-	@RolesAllowed({"VIEW_CONFIGURATIONS","EDIT_CONFIGURATIONS"})
 	Settings get();
 	
 	/**
