a184de37e93dcbf65be9fbeb379abaccb0872aea
Philipp Sigloch <philipp.sigloch@cinovo.de>
2018-08-30 10:22:36 +0200

    introduces variables to configvalues

--- src/main/java/de/cinovo/cloudconductor/api/interfaces/IConfigValue.java
+++ src/main/java/de/cinovo/cloudconductor/api/interfaces/IConfigValue.java
@@ -1,5 +1,8 @@
 package de.cinovo.cloudconductor.api.interfaces;
 
+import de.cinovo.cloudconductor.api.MediaType;
+import de.cinovo.cloudconductor.api.model.ConfigValue;
+
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -8,9 +11,6 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-
-import de.cinovo.cloudconductor.api.MediaType;
-import de.cinovo.cloudconductor.api.model.ConfigValue;
 
 /**
  * Copyright 2017 Cinovo AG<br>
@@ -22,14 +22,14 @@ import de.cinovo.cloudconductor.api.model.ConfigValue;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public interface IConfigValue {
-	
+
 	/**
 	 * @return a collection of known templates
 	 */
 	@GET
 	@RolesAllowed({"VIEW_CONFIGVALUES", "EDIT_CONFIGVALUES"})
 	String[] getAvailableTemplates();
-	
+
 	/**
 	 * Returns configuration of the given template as Key-Value Pairs
 	 *
@@ -41,33 +41,22 @@ public interface IConfigValue {
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_JAVAARGS, MediaType.APPLICATION_JAVAPROPS})
 	@RolesAllowed({"VIEW_CONFIGVALUES", "EDIT_CONFIGVALUES", "USE_AGENT_API"})
 	ConfigValue[] get(@PathParam("template") String template);
-	
+
 	/**
 	 * Deletes all configuration values for the given template.
-	 * 
+	 *
 	 * @param templateName the name of the template
 	 */
 	@DELETE
 	@Path("/{template}")
 	@RolesAllowed({"EDIT_CONFIGVALUES"})
 	void deleteForTemplate(@PathParam("template") String templateName);
-	
-	/**
-	 * Returns all configuration key of a template in a non stacked variant
-	 *
-	 * @param template the template name
-	 * @return array of configuration values
-	 */
-	@GET
-	@Path("/{template}/unstacked")
-	@RolesAllowed({"VIEW_CONFIGVALUES", "EDIT_CONFIGVALUES", "USE_AGENT_API"})
-	ConfigValue[] getUnstacked(@PathParam("template") String template);
-	
+
 	/**
 	 * Returns configuration of the given service within the template as Key-Value Pairs
 	 *
 	 * @param template the template name
-	 * @param service the name of the service
+	 * @param service  the name of the service
 	 * @return array of key value pairs representing the configuration of the service within the template
 	 */
 	@GET
@@ -75,46 +64,46 @@ public interface IConfigValue {
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_JAVAARGS, MediaType.APPLICATION_JAVAPROPS})
 	@RolesAllowed({"VIEW_CONFIGVALUES", "EDIT_CONFIGVALUES", "USE_AGENT_API"})
 	ConfigValue[] get(@PathParam("template") String template, @PathParam("service") String service);
-	
+
 	/**
 	 * Deletes all configuration values for given template and service.
-	 * 
+	 *
 	 * @param template the name of the template
-	 * @param service the name of the service
+	 * @param service  the name of the service
 	 */
 	@DELETE
 	@Path("/{template}/{service}")
 	@RolesAllowed({"EDIT_CONFIGVALUES"})
 	void deleteForService(@PathParam("template") String template, @PathParam("service") String service);
-	
+
 	/**
 	 * Returns the value for a key of the given service within the template as Key-Value Pairs
 	 *
 	 * @param template the template name
-	 * @param service the name of the service
-	 * @param key the name of the key
+	 * @param service  the name of the service
+	 * @param key      the name of the key
 	 * @return the value of the key of the service within the template
 	 */
 	@GET
-	@Path("/{template}/{service" + ":.*}/{key}")
+	@Path("/{template}/{service:.*}/{key}")
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_JAVAARGS, MediaType.APPLICATION_JAVAPROPS})
 	@RolesAllowed({"VIEW_CONFIGVALUES", "EDIT_CONFIGVALUES", "USE_AGENT_API"})
 	String get(@PathParam("template") String template, @PathParam("service") String service, @PathParam("key") String key);
-	
+
 	/**
 	 * Returns the value for a key of the given service within the template as Key-Value Pair, but does no parent matching
 	 *
 	 * @param template the template name
-	 * @param service the name of the service
-	 * @param key the name of the key
+	 * @param service  the name of the service
+	 * @param key      the name of the key
 	 * @return the value of the key of the service within the template
 	 */
 	@GET
-	@Path("/{template}/{service" + ":.*}/{key}/exact")
+	@Path("/{template}/{service:.*}/{key}/exact")
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_JAVAARGS, MediaType.APPLICATION_JAVAPROPS})
 	@RolesAllowed({"VIEW_CONFIGVALUES", "EDIT_CONFIGVALUES", "USE_AGENT_API"})
 	String getExact(@PathParam("template") String template, @PathParam("service") String service, @PathParam("key") String key);
-	
+
 	/**
 	 * Adds a new key-value pair to the configuration of a service within a template
 	 *
@@ -123,17 +112,71 @@ public interface IConfigValue {
 	@PUT
 	@RolesAllowed({"EDIT_CONFIGVALUES"})
 	void save(ConfigValue config);
-	
+
 	/**
 	 * Delete a ConfigValue
 	 *
 	 * @param template the template name
-	 * @param service the name of the service
-	 * @param key the name of the key
+	 * @param service  the name of the service
+	 * @param key      the name of the key
 	 */
 	@DELETE
-	@Path("/{template}/{service" + ":.*}/{key}")
+	@Path("/{template}/{service:.*}/{key}")
 	@RolesAllowed({"EDIT_CONFIGVALUES"})
 	void delete(@PathParam("template") String template, @PathParam("service") String service, @PathParam("key") String key);
-	
+
+
+	/**
+	 * Returns all configuration key of a template in a non stacked variant
+	 *
+	 * @param template the template name
+	 * @return array of configuration values
+	 */
+	@GET
+	@Path("/clean/{template}")
+	@RolesAllowed({"VIEW_CONFIGVALUES", "EDIT_CONFIGVALUES", "USE_AGENT_API"})
+	ConfigValue[] getClean(@PathParam("template") String template);
+
+	/**
+	 * Returns configuration of the given service within the template as Key-Value Pairs
+	 *
+	 * @param template the template name
+	 * @param service  the name of the service
+	 * @return array of key value pairs representing the configuration of the service within the template
+	 */
+	@GET
+	@Path("/clean/{template}/{service}")
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_JAVAARGS, MediaType.APPLICATION_JAVAPROPS})
+	@RolesAllowed({"VIEW_CONFIGVALUES", "EDIT_CONFIGVALUES", "USE_AGENT_API"})
+	ConfigValue[] getClean(@PathParam("template") String template, @PathParam("service") String service);
+
+	/**
+	 * Returns the value for a key of the given service within the template as Key-Value Pairs
+	 *
+	 * @param template the template name
+	 * @param service  the name of the service
+	 * @param key      the name of the key
+	 * @return the value of the key of the service within the template
+	 */
+	@GET
+	@Path("/clean/{template}/{service:.*}/{key}")
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_JAVAARGS, MediaType.APPLICATION_JAVAPROPS})
+	@RolesAllowed({"VIEW_CONFIGVALUES", "EDIT_CONFIGVALUES", "USE_AGENT_API"})
+	String getClean(@PathParam("template") String template, @PathParam("service") String service, @PathParam("key") String key);
+
+
+	/**
+	 * Returns the value for a key of the given service within the template as Key-Value Pair, but does no parent matching
+	 *
+	 * @param template the template name
+	 * @return the value of the key of the service within the template
+	 */
+	@GET
+	@Path("clean/vars/{template}")
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_JAVAARGS, MediaType.APPLICATION_JAVAPROPS})
+	@RolesAllowed({"VIEW_CONFIGVALUES", "EDIT_CONFIGVALUES", "USE_AGENT_API"})
+	ConfigValue[] getCleanVars(@PathParam("template") String template);
 }
+
+
+
